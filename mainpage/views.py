449210da855f78ab52eef2c39f7cd5c83ef7d4a4
Delta <DeltaMail@qq.com>
2022-03-14 14:48:03 +0800

    done logout part

--- mainpage/views.py
+++ mainpage/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
-
+from django.contrib.auth import logout
 # Create your views here.
 
 
 @login_required(login_url='/user/login/')
 def index(request):
     return render(request, 'webim.html')
+
+
+@login_required(login_url='/user/login/')
+def userlogout(request):
+    logout(request)
+    return redirect("/user/assistant/" + "您已经成功退出")
