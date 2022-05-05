40ccab2ec9be825ec00cf47fc3acbdd46fe7ff67
Delta <DeltaMail@qq.com>
2022-05-05 22:34:35 +0800

    chore: Modify Readme.md and docuemnts

--- apis/urls.py
+++ apis/urls.py
@@ -2,16 +2,20 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    # 用户管理模块
+    # login / register / getUserInfo / getCsrfToken / assistant
+
     # 好友管理模块
     path("addFriend", addFriend),
     path("getFriendsList", getFriendsList),
     path("removeFriend", removeFriend),
 
     # 消息管理模块
+    path("getMessagesLength", getMessagesLength),
+    
     path("getMessagesSendByMe", getMessagesSendByMe),
     path("getMessagesSendByFriend", getMessagesSendByFriend),
     path("sendMessage", sendMessage),
-    path("getMessagesLength", getMessagesLength),
     path("getLastMessageSendByFriend", getLastMessageSendByFriend),
 
     # 辅助模块
